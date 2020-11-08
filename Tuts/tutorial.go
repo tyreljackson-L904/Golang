05571b14cee13321d26a22e173726d845cd9f352
Tyrel Jackson <tyreljackson904@gmail.com>
2020-11-07 17:10:45 -0700

    hello world

---
+++ Tuts/tutorial.go
@@ -0,0 +1,10 @@
+package main // should always include this line
+
+import "fmt" // should always include this line
+
+// Create Entry Point - will automatically run when file in range
+func main() {
+
+	fmt.Println("Hello World!") // Tells it to print message to console
+
+}
