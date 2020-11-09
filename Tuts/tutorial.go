e56b46f80226edf7a22b771d86542ee63efc3864
Tyrel Jackson <tyreljackson904@gmail.com>
2020-11-09 00:58:30 -0700

    beginning

--- Tuts/tutorial.go
+++ Tuts/tutorial.go
@@ -4,7 +4,8 @@ import "fmt" // should always include this line
 
 // Create Entry Point - will automatically run when file in range
 func main() {
+	var number uint16 // value name, value type, declaration
 
-	fmt.Println("Hello World!") // Tells it to print message to console
+	fmt.Println("Hello World!", number) // Tells it to print message to console
 
 }
